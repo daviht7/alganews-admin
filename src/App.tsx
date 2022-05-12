3e9a278390108a27cfe942f0b3a2092ed0e70f93
Davi Holanda <daviht7@gmail.com>
2022-05-12 20:30:01 -0300

    Instalando a biblioteca de gráficos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Col, Row, Typography } from 'antd';
 import React from 'react';
+import CompanyMetrics from './app/features/CompanyMetrics';
+
+const { Title, Paragraph } = Typography;
 
 function App() {
-  return <div>app</div>;
+  return (
+    <Row>
+      <Col span={24}>
+        <Title level={2}>Olá, José Souza</Title>
+        <Paragraph>
+          Este é o resumo da empresas nos últimos doze meses
+        </Paragraph>
+      </Col>
+      <Col span={24}>
+        <CompanyMetrics />
+      </Col>
+    </Row>
+  );
 }
 
 export default App;
