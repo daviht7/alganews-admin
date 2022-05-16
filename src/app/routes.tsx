0c9f61962df3e4d2ddc6003a4cb0108fd7fbef16
Davi Holanda <daviht7@gmail.com>
2022-05-16 20:14:38 -0300

    Criando as rotas base

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -5,11 +5,42 @@ import {
   Routes as Switch,
 } from 'react-router-dom';
 
+import CashFlowExpensesView from './views/CashFlowExpenses.view';
+import CashFlowRevenuesView from './views/CashFlowRevenues.view';
+import PaymentCreateView from './views/PaymentCreate.view';
+import PaymentListView from './views/PaymentList.view';
+import UserCreateView from './views/UserCreate.view';
+import UserListView from './views/UserList.view';
+
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path={'/home'} element={<HomeView />} />
+        <Route path={'/'} element={<HomeView />} />
+        <Route
+          path={'/usuarios/criacao'}
+          element={<UserCreateView />}
+        />
+        <Route
+          path={'/usuarios'}
+          element={<UserListView />}
+        />
+        <Route
+          path={'/pagamentos'}
+          element={<PaymentListView />}
+        />
+        <Route
+          path={'/pagamentos/criacao'}
+          element={<PaymentCreateView />}
+        />
+        <Route
+          path={'/fluxo-de-caixa/despesas'}
+          element={<CashFlowExpensesView />}
+        />
+        <Route
+          path={'/fluxo-de-caixa/receitas'}
+          element={<CashFlowRevenuesView />}
+        />
       </Switch>
     </Router>
   );
---
+++ src/app/views/CashFlowRevenues.view.tsx
@@ -0,0 +1,3 @@
+export default function CashFlowRevenuesView() {
+  return <div>CashFlowRevenuesView</div>;
+}
