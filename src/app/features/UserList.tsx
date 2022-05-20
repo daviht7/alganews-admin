0ca7f47a1944e09e0254521156f94422c0e9060a
Davi Holanda <daviht7@gmail.com>
2022-05-20 20:56:10 -0300

    Customizando renderização das colunas da tabela

--- src/app/features/UserList.tsx
+++ src/app/features/UserList.tsx
@@ -1,7 +1,13 @@
-import { Table } from 'antd';
+import { Button, Switch, Table, Tag } from 'antd';
+import { format } from 'date-fns';
 import { User } from 'daviht7-sdk';
 import { useEffect } from 'react';
 import useUsers from '../../core/hooks/useUsers';
+
+import {
+  EyeOutlined,
+  EditOutlined,
+} from '@ant-design/icons';
 
 export default function UserList() {
   const { users, fetchUsers } = useUsers();
@@ -26,18 +32,60 @@ export default function UserList() {
           {
             dataIndex: 'role',
             title: 'Perfil',
+            align: 'center',
+            render(role) {
+              return (
+                <Tag
+                  color={
+                    role === 'MANAGER' ? 'red' : 'success'
+                  }
+                >
+                  {role === 'EDITOR'
+                    ? 'Editor'
+                    : role === 'Manager'
+                    ? 'Gerente'
+                    : 'Assistente'}
+                </Tag>
+              );
+            },
           },
           {
             dataIndex: 'createdAt',
             title: 'Criação',
+            align: 'center',
+            render(createdAt: string) {
+              return format(
+                new Date(createdAt),
+                'dd/MM/yyyy'
+              );
+            },
           },
           {
             dataIndex: 'active',
             title: 'Ativo',
+            align: 'center',
+            render(active: boolean) {
+              return <Switch defaultChecked={active} />;
+            },
           },
           {
             dataIndex: 'id',
             title: 'Ações',
+            align: 'center',
+            render() {
+              return (
+                <>
+                  <Button
+                    size='small'
+                    icon={<EyeOutlined />}
+                  />
+                  <Button
+                    size='small'
+                    icon={<EditOutlined />}
+                  />
+                </>
+              );
+            },
           },
         ]}
       />
