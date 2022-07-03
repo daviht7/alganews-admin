929460566636d39188963ef44ece7d3eebaef6e7
Davi Holanda <daviht7@gmail.com>
2022-07-03 16:29:13 -0300

    Desafio - Label dinâmica no botão do formulário

--- src/app/features/UserForm.tsx
+++ src/app/features/UserForm.tsx
@@ -575,7 +575,9 @@ export default function UserForm(props: UserFormProps) {
         <Col lg={24}>
           <Row justify='end'>
             <Button type={'primary'} htmlType={'submit'}>
-              Cadastrar usuário
+              {props.user
+                ? 'Atualizar Usuário'
+                : 'Cadastrar Usuário'}
             </Button>
           </Row>
         </Col>
